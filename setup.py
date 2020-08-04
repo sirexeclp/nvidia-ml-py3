cf94948da22b98ad49a4fac8b517d58110ec8a01
felix <sirexeclp@gmail.com>
2020-08-04 15:33:56 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,32 @@
 from distutils.core import setup
 from sys import version
 
-# earlier versions don't support all classifiers
-if version < '2.2.3':
-    from distutils.dist import DistributionMetadata
-    DistributionMetadata.classifiers = None
-    DistributionMetadata.download_url = None
+_package_name="pynvml3"
 
-_package_name='nvidia-ml-py3'
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setup(name=_package_name,
-      version='8.440.0',
-      description='Python Bindings for the NVIDIA Management Library',
-      py_modules=['pynvml'],
-      package_data={_package_name: ['Example.txt']},
+      version="8.440.0",
+      description="Python Bindings for the NVIDIA Management Library",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=setuptools.find_packages(),
       license="BSD",
       url="http://www.nvidia.com/",
       author="NVIDIA Corporation",
       author_email="nvml-bindings@nvidia.com",
       classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'Intended Audience :: System Administrators',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: POSIX :: Linux',
-          'Programming Language :: Python',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'Topic :: System :: Hardware',
-          'Topic :: System :: Systems Administration',
+          "Development Status :: 5 - Production/Stable",
+          "Intended Audience :: Developers",
+          "Intended Audience :: System Administrators",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: Microsoft :: Windows",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: System :: Hardware",
+          "Topic :: System :: Systems Administration",
           ],
+      python_requires=">=3.7"
       )
