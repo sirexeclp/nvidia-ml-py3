6bd9c0e469ec86085bd289a0b81c62dfa804b28d
felix <sirexeclp@gmail.com>
2020-08-07 14:29:35 +0200

    easy import

---
+++ pynvml3/__init__.py
@@ -0,0 +1,12 @@
+from pynvml3.device import Device
+from .constraints import PowerLimit, LockedClocks
+from .constants import *
+from .enums import *
+from .errors import *
+from .event_set import EventSet
+from .flags import *
+from .nvlink import NvLink
+from .pynvml import NVMLLib# , NvmlBase
+from .structs import *
+from .system import System
+from .unit import Unit
