a54557246f76286349955e822befc1c2c32d723b
felix <sirexeclp@gmail.com>
2020-08-07 14:56:41 +0200

    fix tests

--- tests/test_pynvml.py
+++ tests/test_pynvml.py
@@ -1,50 +1,54 @@
 from datetime import datetime, timedelta
 from unittest import TestCase
 
-from pynvml import NVMLLib
-from system import System
-from device import Device
+from pynvml3 import FieldId, ValueType, InfoRom
+from pynvml3.pynvml import NVMLLib
+from pynvml3.system import System
+from pynvml3.device import Device
+from psutil import Process
 
 
 class TestDevice(TestCase):
     def test_from_index(self):
-        with NVMLLib():
-            device = Device.from_index(0)
-            # print(device.get_name())
-            # print(device.get_board_id())
-            # print(device.get_brand())
-            # print(device.get_multi_gpu_board())
-            # print("affinity", device.get_cpu_affinity(2)[0])
-            # print("Minor", device.get_minor_number())
-            # uuid = device.get_uuid()
-            # print(uuid)
-            # print(type(uuid))
-            # print(type(uuid.encode("ASCII")))
-            # print(uuid.encode("ASCII"))
-            # # device = Device.from_uuid(uuid.encode("ASCII"))
-            # # print(device.get_brand())
-            # print(device.get_cuda_compute_capability())
-            # # print(device.get_serial())
-            # #print(device.get_inforom_version(InfoRom.ECC))
-            # print("total energy", device.get_total_energy_consumption()/1_000_000)
-            # print(device.query_drain_state())
+        with NVMLLib() as lib:
+            device = lib.device.from_index(0)
+            print(device.get_name())
+            print(device.get_board_id())
+            print(device.get_brand())
+            print(device.get_multi_gpu_board())
+            print("affinity", device.get_cpu_affinity()[0])
+            print("Minor", device.get_minor_number())
+            uuid = device.get_uuid()
+            print(uuid)
+            device = lib.device.from_uuid(uuid)
+            print(device.get_brand())
+            print(device.get_cuda_compute_capability())
+            # print(device.get_serial())
+            # print(device.get_inforom_version(InfoRom.ECC))
+            print("total energy", device.get_total_energy_consumption()/1_000_000)
 
-            # print(device.modify_drain_state(EnableState.FEATURE_ENABLED))
-            # print(device.query_drain_state())
             pci_info = device.get_pci_info()
             print(pci_info.query_drain_state())
             # Device.modify_drain_state(pci_info, EnableState.FEATURE_DISABLED)
             # print(Device.discover_gpus(PciInfo()))
 
     def test_nvlink(self):
-        with NVMLLib():
-            device = Device.from_index(0)
-            for i in NvLinkCapability:
-                print(str(i), device.get_nv_link_capability(0, i))
+        with NVMLLib() as lib:
+            device = lib.device.from_index(0)
+            gpu_processes = device.get_graphics_running_processes()
+            processes = [Process(x.pid) for x in gpu_processes]
+            paths = [p.cmdline()[0] + "/" + p.name() for p in processes]
+            print(paths)
+            print([p.usedGpuMemory / 2 ** 20 for p in gpu_processes])
+
+            print(device.get_minor_number())
+
+            # for i in NvLinkCapability:
+            #     print(str(i), device.get_nv_link_capability(0, i))
 
     def test_nvml_device_get_field_values(self):
-        with NVMLLib():
-            device = Device.from_index(0)
+        with NVMLLib() as lib:
+            device = lib.device.from_index(0)
             values = device.get_field_values(1, FieldId.TOTAL_ENERGY_CONSUMPTION)
             ts = timedelta(microseconds=values.timestamp) + datetime.fromtimestamp(0)
             print("TimeStamp", ts)
@@ -53,13 +57,13 @@ class TestDevice(TestCase):
             print("total_energy", device.get_total_energy_consumption())
 
     def test_system(self):
-        with NVMLLib():
-            system = System()
+        with NVMLLib() as lib:
+            system = lib.system
             print("driver-version", system.get_driver_version())
             print("cuda-driver-version", system.get_cuda_driver_version())
             # print("proc-name", system.get_process_name())
             print(system.get_topology_gpu_set(0))
-            device = Device(system.get_topology_gpu_set(0)[0])
+            device = Device(lib, system.get_topology_gpu_set(0)[0])
             print(device.get_name())
             print(device.get_supported_event_types())
             # events = device.register_events(EventType.PState)
@@ -67,6 +71,6 @@ class TestDevice(TestCase):
             # print("data", data.eventData)
             # print("fan-speed", device.get_fan_speed())
             # print(device.get_supported_memory_clocks())
-            #print("Unitcount", Unit.get_count())
-            #print("unitcount", Unit(0).get_psu_info())
+            # print("Unitcount", Unit.get_count())
+            # print("unitcount", Unit(0).get_psu_info())
             print("aff", [bin(x) for x in device.get_cpu_affinity()])
