76238b334f86ac3a3e8052fddbd55bcbe67166c9
felix <sirexeclp@gmail.com>
2020-08-12 17:22:12 +0200

    remove locks; locks are for loosers

--- pynvml3/pynvml.py
+++ pynvml3/pynvml.py
@@ -1,104 +1,66 @@
-#####
-# Copyright (c) 2011-2015, NVIDIA Corporation.  All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#    * Redistributions of source code must retain the above copyright notice,
-#      this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above copyright
-#      notice, this list of conditions and the following disclaimer in the
-#      documentation and/or other materials provided with the distribution.
-#    * Neither the name of the NVIDIA Corporation nor the names of its
-#      contributors may be used to endorse or promote products derived from
-#      this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-# THE POSSIBILITY OF SUCH DAMAGE.
-#####
+################################################################################
+# Copyright (c) 2011-2015, NVIDIA Corporation.  All rights reserved.           #
+#                                                                              #
+# Redistribution and use in source and binary forms, with or without           #
+# modification, are permitted provided that the following conditions are met:  #
+#                                                                              #
+#    * Redistributions of source code must retain the above copyright notice,  #
+#      this list of conditions and the following disclaimer.                   #
+#    * Redistributions in binary form must reproduce the above copyright       #
+#      notice, this list of conditions and the following disclaimer in the     #
+#      documentation and/or other materials provided with the distribution.    #
+#    * Neither the name of the NVIDIA Corporation nor the names of its         #
+#      contributors may be used to endorse or promote products derived from    #
+#      this software without specific prior written permission.                #
+#                                                                              #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  #
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE    #
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE   #
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE    #
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR          #
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF         #
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS     #
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN      #
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)      #
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF       #
+# THE POSSIBILITY OF SUCH DAMAGE.                                              #
+################################################################################
 
-from ctypes import *
-import sys
 import os
-import threading
+import sys
+from ctypes import *
+from functools import lru_cache
 from pathlib import Path
 from typing import List
-import typing
-from abc import ABC
 
-from pynvml3.event_set import EventSet
-from pynvml3.unit import CUnitPointer, Unit
-from pynvml3.system import System
 from pynvml3.device import Device, CDevicePointer
-from pynvml3.errors import NVMLErrorFunctionNotFound, NVMLErrorSharedLibraryNotFound, Return
-
-
-
-
-# On Windows with the WDDM driver, usedGpuMemory is reported as None
-# Code that processes this structure should check for None, I.E.
-#
-# if (info.usedGpuMemory == None):
-#     # TODO handle the error
-#     pass
-# else:
-#    print("Using %d MiB of memory" % (info.usedGpuMemory / 1024 / 1024))
-#
-# See NVML documentation for more information
-
-#################################
-#        NvLink Methods         #
-#################################
+from pynvml3.errors import NVMLErrorFunctionNotFound,\
+    NVMLErrorSharedLibraryNotFound, Return
+from pynvml3.event_set import EventSet
+from pynvml3.system import System
+from pynvml3.unit import CUnitPointer, Unit
 
 
 class NVMLLib:
     """Methods that handle NVML initialization and cleanup."""
-    lock = threading.RLock()
-    refcount = 0
-    _instance = None
-
-    def __new__(cls, *args, **kwargs):
-        with cls.lock:
-            if not cls._instance:
-                cls._instance = super().__new__(cls, *args, **kwargs)
-            return cls._instance
 
     def __init__(self):
-        self.function_pointer_cache = {}
+        """Load the library."""
         self.nvml_lib = None
         self._load_nvml_library()
 
     def __enter__(self):
-        # Initialize the library
+        """Initialize the library."""
         fn = self.get_function_pointer("nvmlInit_v2")
         ret = fn()
         Return.check(ret)
-
-        # Atomically update refcount
-        with self.lock:
-            self.refcount += 1
-
         return self
 
     def __exit__(self, *argc, **kwargs):
-        # Leave the library loaded, but shutdown the interface
+        """Leave the library loaded, but shutdown the interface."""
         fn = self.get_function_pointer("nvmlShutdown")
         ret = fn()
         Return.check(ret)
-
-        # Atomically update refcount
-        with self.lock:
-            if self.refcount > 0:
-                self.refcount -= 1
 
     def open(self) -> None:
         """Initialize the library.
@@ -115,45 +77,42 @@ class NVMLLib:
         self.__exit__()
 
     def _load_nvml_library(self) -> None:
-        """
-        Load the library if it isn't loaded already
-        """
-        with self.lock:
-            try:
-                if sys.platform[:3] == "win":
-                    search_paths = self._get_search_paths()
-                    nvml_path = next((x for x in search_paths if x.is_file()), None)
-                    if nvml_path is None:
-                        raise NVMLErrorSharedLibraryNotFound
-                    else:
-                        # cdecl calling convention
-                        self.nvml_lib = CDLL(str(nvml_path))
+        """Load the library."""
+        try:
+            if sys.platform[:3] == "win":
+                search_paths = self._get_search_paths()
+                nvml_path = next((x for x in search_paths if x.is_file()), None)
+                if nvml_path is None:
+                    raise NVMLErrorSharedLibraryNotFound
                 else:
-                    # assume linux
-                    self.nvml_lib = CDLL("libnvidia-ml.so.1")
-            except OSError as ose:
-                raise NVMLErrorSharedLibraryNotFound
-            if self.nvml_lib is None:
-                raise NVMLErrorSharedLibraryNotFound
+                    # cdecl calling convention
+                    self.nvml_lib = CDLL(str(nvml_path))
+            else:
+                # assume linux
+                self.nvml_lib = CDLL("libnvidia-ml.so.1")
+        except OSError as ose:
+            raise NVMLErrorSharedLibraryNotFound
+        if self.nvml_lib is None:
+            raise NVMLErrorSharedLibraryNotFound
 
     @staticmethod
     def _get_search_paths() -> List[Path]:
+        """Computes search paths for the library on Windows."""
         program_files = Path(os.getenv("ProgramFiles", r"C:\Program Files"))
         win_dir = Path(os.getenv("WinDir", r"C:\Windows"))
         paths = [program_files / r"NVIDIA Corporation\NVSMI\nvml.dll",
                  win_dir / r"System32\nvml.dll"]
         return paths
 
-    def get_function_pointer(self, name) -> "ctypes.CDLL.__init__.<locals>._FuncPtr":
-        if name in self.function_pointer_cache:
-            return self.function_pointer_cache[name]
-
-        with self.lock:
-            try:
-                self.function_pointer_cache[name] = getattr(self.nvml_lib, name)
-                return self.function_pointer_cache[name]
-            except AttributeError:
-                raise NVMLErrorFunctionNotFound
+    @lru_cache(maxsize=None)
+    def get_function_pointer(self, name: str) -> "ctypes.CDLL.__init__.<locals>._FuncPtr":
+        """Returns a function pointer for the given function name.
+        Caching is used for YOUR convenience.
+        """
+        try:
+            return getattr(self.nvml_lib, name)
+        except AttributeError:
+            raise NVMLErrorFunctionNotFound
 
     @property
     def unit(self) -> "UnitFactory":
@@ -272,12 +231,33 @@ class DeviceFactory:
         return c_count.value
 
     def from_index(self, index: int) -> "Device":
-        """
+        """Acquire the handle for a particular device, based on its index.
 
-        @param index:
-        @type index:
-        @return:
-        @rtype: Device
+        Valid indices are derived from the accessibleDevices count returned by
+        :func:`DeviceFactory.get_count` .
+        For example, if accessibleDevices is 2 the valid indices are 0 and 1,
+        corresponding to GPU 0 and GPU 1.
+
+        The order in which NVML enumerates devices has no guarantees
+        of consistency between reboots.
+        For that reason it is recommended that devices be looked
+        up by their PCI ids or UUID.
+        See :func:`DeviceFactory.from_uuid` and
+        :func:`DeviceFactory.from_pci_bus_id`.
+
+        Note:
+            The NVML index may not correlate with other APIs,
+            such as the CUDA device index.
+
+        Starting from NVML 5, this API causes NVML to initialize the target
+        GPU NVML may initialize additional GPUs,
+        if the target GPU is an SLI slave.
+
+        Args:
+            index: The index of the target GPU, >= 0 and < accessibleDevices
+
+        Returns: the device object
+
         """
         c_index = c_uint(index)
         handle = CDevicePointer()
